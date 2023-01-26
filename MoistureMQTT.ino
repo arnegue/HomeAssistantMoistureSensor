6b47fb0704c8bf66a617404d82e16912ec356426
Arne <arne-@hotmail.de>
2023-01-26 17:23:59 +0100

    Removed wake-up-reason-calling, removed debug-printing, fixed delays, moisture sensing now working

--- MoistureMQTT.ino
+++ MoistureMQTT.ino
@@ -1,29 +1,23 @@
 #include "wifi_settings.h"
 #include "WiFi.h"
 
-#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+#define uS_TO_S_FACTOR                      1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP_S                     5        /* Time ESP32 will go to sleep (in seconds) */
+#define SENSOR_POLL_SLEEP_TIME_MS           50 /* Time to sleep when measuring values */
+#define MOISTURE_SENSOR_POWER_UP_TIME_MS    250
 
-//RTC_DATA_ATTR int bootCount = 0;
+#define BATTERY_ANALOG_PIN          34
+#define MOISTURE_SENSOR_ANALOG_PIN  35
+#define MOISTURE_SENSOR_POWER_PIN   32
 
-void print_wakeup_reason(){
-  esp_sleep_wakeup_cause_t wakeup_reason;
-
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-
-  switch(wakeup_reason)
-  {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
-  }
+void setup_pins() {
+  pinMode(BATTERY_ANALOG_PIN, INPUT);
+  pinMode(MOISTURE_SENSOR_ANALOG_PIN, INPUT);
+  pinMode(MOISTURE_SENSOR_POWER_PIN, OUTPUT);
 }
 
 void setup_wifi(){
-    Serial.println("Trying to connect to WIFI_SSID with WIFI_PW");
+    Serial.println("Trying to connect to " WIFI_SSID " with pw " WIFI_PW);
     WiFi.begin(WIFI_SSID, WIFI_PW);
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -41,39 +35,48 @@ void send_to_mqtt(int moisture, int voltage) {
 }
 
 /// Takes x measurement-point in y seconds. Returns average of x measurments
-int measure_soil_moisture() {
+int read_analog_values(int pin) {
   int meas_points = 10;
   int sum_values = 0;
   int meas_value = 0;
   for(int i=0; i < meas_points; i++) {
-    meas_value = 2; // TODO read analog value
-    Serial.print("Measured value: ");
-    Serial.println(meas_value);
+    meas_value = analogRead(pin);
+    //Serial.print("Measured value: ");
+    //Serial.println(meas_value);
     sum_values += meas_value;  // TODO int-overflow?
-    // TODO sleep
+    delay(SENSOR_POLL_SLEEP_TIME_MS);
   }
   return sum_values / meas_points;
 }
 
+int measure_soil_moisture() {
+  int value;
+  digitalWrite(MOISTURE_SENSOR_POWER_PIN, HIGH);
+  delay(MOISTURE_SENSOR_POWER_UP_TIME_MS);
+  value = read_analog_values(MOISTURE_SENSOR_ANALOG_PIN);
+  digitalWrite(MOISTURE_SENSOR_POWER_PIN, LOW); 
+  Serial.print("Moisture:");
+  Serial.println(value);
+  return value;
+}
+
 int get_battery_voltage() {
-  // TODO read analog value
+  return read_analog_values(BATTERY_ANALOG_PIN);
 }
 
 void setup(){
   Serial.begin(115200);
+  
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP_S * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP_S) + " Seconds");
 
-  print_wakeup_reason();
-
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
-
-  setup_wifi();
+  setup_pins();
+  //setup_wifi();
   setup_mqtt();
 
   int moisture = measure_soil_moisture();
   int voltage = get_battery_voltage();
   send_to_mqtt(moisture, voltage);
-
 
   Serial.println("Going to sleep now");
   Serial.flush(); 
@@ -82,6 +85,5 @@ void setup(){
 }
 
 void loop(){
-  //This is not going to be called
 }
 
