c3fc678affdfe6fdce0ab497d35f5a91a245eb95
Arne <arne-@hotmail.de>
2023-01-27 13:53:54 +0100

    Better handling with percentatages

--- MoistureMQTT.ino
+++ MoistureMQTT.ino
@@ -3,20 +3,6 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-const char broker[] = "192.168.178.28";
-const int mqtt_port = 1883;
-WiFiClient client;
-Adafruit_MQTT_Client mqtt_client(&client, broker, mqtt_port);
-
-Adafruit_MQTT_Publish mqtt_battery  = Adafruit_MQTT_Publish(&mqtt_client, "MoistureSensor/battery");
-Adafruit_MQTT_Publish mqtt_moisture = Adafruit_MQTT_Publish(&mqtt_client, "MoistureSensor/moisture");
-
-// TODO Possible optimization: Measure battery and moisture at once
-// TODO Possible optimization: Put wifi-status just before publishing to mqtt instead of using time for measuring values
-// TODO calibration? Or work later on raw data? https://makersportal.com/blog/2020/5/26/capacitive-soil-moisture-calibration-with-arduino 
-// TODO              Work with MAX_MOISTURE_RAW and MIN_MOISTURE_RAW calc percentage and live with that (should be enough) -> is 100% dry or wet?
-// TODO battery: Measure 3 volts and use this as 100% if over, just use the 3 volt!
-
 #define uS_TO_S_FACTOR 1000000       /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP_S 5            /* Time ESP32 will go to sleep (in seconds) */
 #define SENSOR_POLL_SLEEP_TIME_MS 50 /* Time to sleep when measuring values */
@@ -26,12 +12,23 @@ Adafruit_MQTT_Publish mqtt_moisture = Adafruit_MQTT_Publish(&mqtt_client, "Moist
 #define MOISTURE_SENSOR_ANALOG_PIN 35
 #define MOISTURE_SENSOR_POWER_PIN 32
 
-#define MAX_MOISTURE_RAW = 123  // Measure the raw value in oversaturated soil and put it here
-#define MIN_MOISTURE_RAW = 0    // Measure the raw value in dry soil and put it here
+#define MAX_MOISTURE_RAW 1000  // Measure the raw value in oversaturated soil and put it here
+#define MIN_MOISTURE_RAW 2400    // Measure the raw value in dry soil and put it here
 
+#define MAX_VOLTAGE_RAW 0xFFF // DAC is 12bit resolution. So that's the maximum for measuring 3.3V
+#define MQTT_TOPIC "homeassistant/sensor/MoistureSensor"
+#define DEVICE_ID "1"
 
-#define MAX_VOLTAGE_RAW = 123  // Measure 3 V(or 3.3) and put raw value here
+const char broker[] = "192.168.178.28";
+const int mqtt_port = 1883;
 
+WiFiClient client;
+Adafruit_MQTT_Client mqtt_client(&client, broker, mqtt_port);
+
+Adafruit_MQTT_Publish mqtt_battery  = Adafruit_MQTT_Publish(&mqtt_client, MQTT_TOPIC DEVICE_ID "/battery");
+Adafruit_MQTT_Publish mqtt_moisture = Adafruit_MQTT_Publish(&mqtt_client, MQTT_TOPIC DEVICE_ID "/moisture");
+
+// TODO Possible optimization: Measure battery and moisture at once
 
 void setup_pins() {
   pinMode(BATTERY_ANALOG_PIN, INPUT);
@@ -42,6 +39,9 @@ void setup_pins() {
 void setup_wifi() {
   Serial.println("Trying to connect to " WIFI_SSID " with pw " WIFI_PW);
   WiFi.begin(WIFI_SSID, WIFI_PW);
+}
+
+void wait_for_wifi() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
@@ -53,7 +53,7 @@ void setup_mqtt() {
   int8_t ret;
   while ((ret = mqtt_client.connect()) != 0) {  // connect will return 0 for connected
     Serial.println(mqtt_client.connectErrorString(ret));
-    Serial.println("Retrying MQTT connection in 5 seconds...");
+    Serial.println("Retrying MQTT connection in 2.5 seconds...");
     mqtt_client.disconnect();
     delay(2500);  // wait 2.5 seconds
   }
@@ -92,21 +92,49 @@ int read_analog_values(int pin) {
   return sum_values / meas_points;
 }
 
+int get_percentage(int value, int max_value, int min_value) {
+  int temp_max = max_value;
+  int temp_min = min_value;
+  if (temp_min > temp_max) {
+      temp_max = min_value;
+      temp_min = max_value;
+  }
+  if (value >= temp_max) {
+    return 100;
+  } else if (value <= temp_min) {
+    return 0;
+  } else {
+    return ((value - temp_max) * 100) / (temp_min - temp_max);  // TODO maybe use a better way without signed int
+  }
+}
+
 int measure_soil_moisture() {
   int value;
   digitalWrite(MOISTURE_SENSOR_POWER_PIN, HIGH);
   delay(MOISTURE_SENSOR_POWER_UP_TIME_MS);
   value = read_analog_values(MOISTURE_SENSOR_ANALOG_PIN);
   digitalWrite(MOISTURE_SENSOR_POWER_PIN, LOW);
-  Serial.print("Moisture:");
+  
+  Serial.print("Moisture_raw:");
+  Serial.print(value);
+
+  Serial.print(",Moisture_p:");
+  value = get_percentage(value, MAX_MOISTURE_RAW, MIN_MOISTURE_RAW);
   Serial.println(value);
+
   return value;
 }
 
 int get_battery_voltage() {
-  int value = random(0, 100); // TODO  read_analog_values(BATTERY_ANALOG_PIN);
-  Serial.print("Voltage:");
+  int value = read_analog_values(BATTERY_ANALOG_PIN);
+
+  Serial.print("Voltage_raw:");
+  Serial.print(value);
+
+  Serial.print(",Voltage_p:");
+  value = get_percentage(value, MAX_VOLTAGE_RAW, 0);
   Serial.println(value);
+
   return value;
 }
 
@@ -118,10 +146,11 @@ void setup() {
 
   setup_pins();
   setup_wifi();
-  setup_mqtt();
 
   int moisture = measure_soil_moisture();
   int voltage = get_battery_voltage();
+  wait_for_wifi();
+  setup_mqtt();
   send_to_mqtt(moisture, voltage);
 
   Serial.println("Going to sleep now");
